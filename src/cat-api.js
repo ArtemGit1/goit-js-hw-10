98ad7744dc6b7db8ee6dd4464bd9f72407440b3d
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-09-22 17:13:08 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] = 'live_Qv8jLCwOXni6K6lhgU21hZm70a6LtqKniDUmXrSzz8rrb6KRoGJytLJW5flEiEzb';
 
-// Function to fetch breeds
+
 export async function fetchBreeds() {
   try {
     const response = await axios.get('https://api.thecatapi.com/v1/breeds');
